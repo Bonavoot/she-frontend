5095485ba0be363f9ec0fd3ab1a0b70e37e8e825
Joseph <Jbonventre95@gmail.com>
2023-02-14 22:24:03 -0500

    updated chart placeholder

--- src/StockChart.jsx
+++ src/StockChart.jsx
@@ -1,7 +1,10 @@
+import stockChartImage from "./assets/stock-chart-5-year.png"
 const StockChart = () => {
 
     return (
-        <div className="stock-chart">STOCK CHART PLACEHOLDER</div>
+        <div className="stock-chart">
+            <img className="stock-chart"  src={stockChartImage} alt="chart" />
+        </div>
     )
 }
 
