526dabde7ed11946103e9da6dde7ecadb0227568
JamesPrawdzik <sebaprawdzik@gmail.com>
2023-02-15 16:38:24 -0500

    jjj frontend

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,25 @@
-import { useState } from 'react'
+
 import './index.css'
-import Home from './Home'
+// import Home from './Home'
+import { useState, useEffect } from 'react'
+import Signup from './Signup'
 
 function App() {
+  const [user, setUser] = useState(null);
 
-  return (
-    <div className="App">
-      <Home />
-    </div>
-  )
+  useEffect(() => {
+    fetch("/me").then((response) => {
+      if (response.ok) {
+        response.json().then((user) => setUser(user));
+      }
+    });
+  }, []);
+
+  if (user) {
+    return <h2>Welcome, {user.username}!</h2>;
+  } else {
+    return <Signup onLogin={setUser} />;
+  }
 }
 
 export default App
--- src/Home.jsx
+++ src/Home.jsx
@@ -2,7 +2,7 @@ import NavBar from "./NavBar"
 import StockChart from "./StockChart"
 import StockInfo from "./StockInfo"
 import StockList from "./StockList"
-import stockChartImage from "./assets/stock-chart-5-year.png"
+
 const Home = () => {
 
     return (
